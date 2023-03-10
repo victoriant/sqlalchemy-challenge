f4cdf38d86916a099947c59aee335f6bcad3a1d4
Tanner Victorian <119895467+victoriant@users.noreply.github.com>
2023-03-09 22:28:09 -0500

    Add files via upload

---
+++ SurfsUp/Climate_App.py
@@ -0,0 +1,82 @@
+import datetime as dt
+import numpy as np
+import pandas as pd
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
+
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+Measurements = Base.classes.measurement
+Station = Base.classes.station
+
+session = Session(engine)
+
+app =Flask(__name__)
+
+@app.route("/")
+def welcome():
+    return ( 
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/start<br/>"
+        f"/api/v1.0/start/end<br/>"
+    )
+
+@app.route("/api/v1.0/precipitation")
+
+def precipitation():
+   prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+   precipitation = session.query(Measurements.date, Measurements.prcp).\
+    filter(Measurements.date >= prev_year).all()
+   precip = {date: prcp for date, prcp in precipitation}
+   return jsonify(precip)    
+
+@app.route("/api/v1.0/stations")
+def stations():
+    stations_query = session.query(Station.name, Station.station)
+    stations = pd.read_sql(stations_query.statement, stations_query.session.bind)
+    return jsonify(stations.to_dict())
+
+@app.route("/api/v1.0/tobs")
+
+def temp_monthly():
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    results = session.query(Measurements.tobs).\
+      filter(Measurements.station == 'USC00519281').\
+      filter(Measurements.date >= prev_year).all()
+    temps = list(np.ravel(results))
+    return jsonify(temps=temps)
+
+@app.route("/api/v1.0/start")
+def trip1(start):
+    start_date= dt.datetime.strptime(start, '%Y-%m-%d')
+    last_year = dt.timedelta(days=365)
+    start = start_date-last_year
+    end =  dt.date(2017, 8, 23)
+    trip_data = session.query(func.min(Measurements.tobs), func.avg(Measurements.tobs), func.max(Measurements.tobs)).\
+        filter(Measurements.date >= start).filter(Measurements.date <= end).all()
+    trip = list(np.ravel(trip_data))
+    return jsonify(trip)
+
+
+@app.route("/api/v1.0/start/end")
+def trip2(start,end):
+    start_date= dt.datetime.strptime(start, '%Y-%m-%d')
+    end_date= dt.datetime.strptime(end,'%Y-%m-%d')
+    last_year = dt.timedelta(days=365)
+    start = start_date-last_year
+    end = end_date-last_year
+    trip_data = session.query(func.min(Measurements.tobs), func.avg(Measurements.tobs), func.max(Measurements.tobs)).\
+        filter(Measurements.date >= start).filter(Measurements.date <= end).all()
+    trip = list(np.ravel(trip_data))
+    return jsonify(trip)
+
+if __name__ == "__main__":
+    app.run(debug=True)
